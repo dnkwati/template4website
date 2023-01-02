be5bbbec6168ffa8ba7afc04a827ac91e3a897ce
divine <drnkwati@yahoo.com>
2023-01-03 03:00:48 +0400

    Initial commit

--- src/exe/preinstall.js
+++ src/exe/preinstall.js
@@ -9,10 +9,10 @@ const repos = {
 	[`${rootDir}/ts`]: 'https://github.com/dnkwati/template4ts.git',
 };
 // const copyConfigs = ['tsconfig.json'];
-// const removeGlobs = ['.*', '*lock*'];
+// const removeGlobs = ['.git', '*lock*'];
 //
 for (const [dir, url] of Object.entries(repos)) {
-	// const removeCommand = removeGlobs.map((val) => ''.concat('rm -rf ', dir, '/', val)).join(' && ');
+	// const removeCommand = removeGlobs.map((val) => 'rm -rf '.concat(dir, '/', val)).join(' && ');
 	// const copyCommand = copyConfigs.map((val) = 'cp -rf '.concat(dir, '/_/', val, ' ', dir)).join(' && ');
 	const log = `[cloning...] ${dir}: ${url}`;
 	const commands = [
@@ -22,6 +22,8 @@ for (const [dir, url] of Object.entries(repos)) {
 		`cp -rf ${dir}/_/src/ ${dir}`,
 		// 3. remove temp folder
 		`rm -rf ${dir}/_`,
+		// 3.1 remove git folder
+		`rm -rf .git`,
 	].join(' && ');
 	//
 	if (!fs.existsSync(dir)) {
