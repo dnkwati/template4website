12ac54d8a523c0bb11b8a41d447d66356226c148
divine <drnkwati@yahoo.com>
2023-01-03 02:07:45 +0400

    Initial commit

--- src/ts/core/greeter.ts
+++
@@ -1,4 +0,0 @@
-//
-export default function greeter(person: string): string {
-	return 'Hello ' + person;
-}
--- src/ts/index.ts
+++
@@ -1,8 +0,0 @@
-// export * from './core/greeter.js';
-import greeter from './core/greeter.js';
-// old school JS
-(function (root) {
-	const node = document.createElement('div');
-	node.textContent = greeter('Jane Doe');
-	document.body.appendChild(node);
-})(this);
